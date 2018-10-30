58777422b348594e9235ba17698343e8d7cf0729
NlGHT <NlGHT@users.noreply.github.com>
2018-10-30 10:48:00 +0100

    Merge branch 'master' of https://github.com/Ivosama/roboFriend
    hopefully this doesn't fuck anything

--- Main.py
+++ Main.py
@@ -6,40 +6,49 @@
 #         / \
 #        /   \
 
-import io
-import picamera
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
 import cv2
-import numpy
+import io
 
-#Create a memory stream so photos doesn't need to be saved in a file
+# Create a memory stream so photos doesn't need to be saved in a file
 stream = io.BytesIO()
 
-#Get the picture (low resolution, so it should be quite fast)
-#Here you can also specify other parameters (e.g.:rotate the image)
-with picamera.PiCamera() as camera:
-    camera.resolution = (320, 240)
-    camera.capture(stream, format='jpeg')
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.color_effects = (128, 128)  # his sets the channels to capture only black and white
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
-#Convert the picture into a numpy array
-buff = numpy.fromstring(stream.getvalue(), dtype=numpy.uint8)
-
-#Now creates an OpenCV image
-image = cv2.imdecode(buff, 1)
-
-#Load a cascade file for detecting faces
+# Load a cascade file for detecting faces
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
-#Convert to grayscale
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+# allow the camera to warmup
+time.sleep(0.1)
 
-#Look for faces in the image using the loaded cascade file
-faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
 
-print ("Found "+str(len(faces))+" face(s)")
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#Draw a rectangle around every found face
-for (x,y,w,h) in faces:
-    cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
-#Save the result image
-cv2.imwrite('result.jpg',image)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    # show the frame
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
--- test_video.py
+++ test_video.py
@@ -3,12 +3,20 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+import io
+
+# Create a memory stream so photos doesn't need to be saved in a file
+stream = io.BytesIO()
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
+camera.color_effects = (128, 128)  # his sets the channels to capture only black and white
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# Load a cascade file for detecting faces
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
 # allow the camera to warmup
 time.sleep(0.1)
@@ -18,6 +26,13 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     # grab the raw NumPy array representing the image, then initialize the timestamp
     # and occupied/unoccupied text
     image = frame.array
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
     # show the frame
     cv2.imshow("Frame", image)
