f5f573d6a5f9c75ecccb150ba8d92fb0665cf11a
ivand <ivandts@gmail.com>
2018-11-13 19:29:41 +0100

    Threshold test with frames

--- test_cv.py
+++ test_cv.py
@@ -1,22 +1,25 @@
 import cv2
+import numpy as np
+import thresholding
 
-#Load an image from file
-image = cv2.imread("face.jpg")
+cap = cv2.VideoCapture(0)
 
-#Load a cascade file for detecting faces
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
-#Convert to grayscale
-gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+while True:
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-#Look for faces in the image using the loaded cascade file
-faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
-print ("Found "+str(len(faces))+" face(s)")
+    for (x, y, w, h) in faces:
+        cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        thresholding.th(gray, x, y, (w + x), (h + y), 0)
 
-#Draw a rectangle around every found face
-for (x,y,w,h) in faces:
-    cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),2)
 
-#Save the result image
-cv2.imwrite('result.jpg',image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
--- thresholding.py
+++ thresholding.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 
-def thresholding(img, stX, stY, endX, endY):
+def th(img, stX, stY, endX, endY, thMod):
     height, width = img.shape
     bI = np.ones((height, width), np.uint8)
     sumPix = 0
@@ -14,16 +14,15 @@ def thresholding(img, stX, stY, endX, endY):
             sumPix += img.item(i, j)
 
     # Now calculates the prefered thresholding value from sum of pixels divided by the resolution
-    TVa = sumPix / ((endX - stX) * (endY - stY))
+    TVa = sumPix / ((endX - stX) * (endY - stY)) + thMod
 
     # Loop through image and set the pixels to be either black or white
     for i in range(stY, endY):
         for j in range(stX, endX):
             if img.item(i, j) >= TVa:
-                bI[i, j] = 255
-            elif img.item(i, j) < TVa:
                 bI[i, j] = 0
-            q += 1
+            elif img.item(i, j) < TVa:
+                bI[i, j] = 255
 
     cv2.imshow('Thresholded Image', bI)
     print(TVa)
