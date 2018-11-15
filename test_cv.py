ff6d6f0db34cdd2e6fa08e4faa0f6eba0832ec9e
ivand <ivandts@gmail.com>
2018-11-15 10:56:05 +0100

    push to main of functions kinda working

--- test_cv.py
+++ test_cv.py
@@ -1,22 +1,31 @@
 import cv2
 import numpy as np
 import thresholding
+import MedianGBlur
+import Detect
+import sys
 
 cap = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+blobDetector = Detect.BlobDetector()
+sys.setrecursionlimit(3000)
 
 while True:
     ret, frame = cap.read()
+    #frame = cv2.imread("TestImages/Straight Outta CREATE 2.png", cv2.IMREAD_COLOR)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    editedImage = np.zeros((frame.shape[0], frame.shape[1]), np.uint8)
 
     faces = face_cascade.detectMultiScale(gray, 1.1, 5)
 
     for (x, y, w, h) in faces:
         cv2.rectangle(gray, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        thresholding.th(gray, x, y, (w + x), (h + y), 0)
+        editedImage = MedianGBlur.medianBlur(gray, editedImage, 3, x, y, w, h)
+        editedImage = thresholding.th(editedImage, x, y, (w + x), (h + y), 0)
+        editedImage = blobDetector.getObjectImage(editedImage, y, y+h, x, x+w, 50, 100)
 
-
+    cv2.imshow("imshow", editedImage)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
