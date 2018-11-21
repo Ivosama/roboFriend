dac6bfc47bc54282a128e862131470dc177ed9da
AnimeBoyWithWindStick <zephan.kav@gmail.com>
2018-11-21 11:55:13 +0100

    slight changes to stuff

--- Reactions.py
+++ Reactions.py
@@ -2,41 +2,43 @@ class Reactions:
     import cv2
     import numpy as np
     import collections
+    import random
     memlen = 10 # robot's memory length for mouth and brow lists, first in first out
     ma = collections.deque(maxlen=memlen)   # Mouth memory
     ba = collections.deque(maxlen=memlen)   # brow memory
-    MOUTH = 'smile'
-    BROW = 'neutral'
-    ma.append(MOUTH)    # Put these where the mouth / brow are being detected
-    ba.append(BROW)
+    ml = ['smile', 'neutral', 'frown']  # list of mouths
+    bl = ['b1', 'b2', 'b3'] # list of brows
+
 
     def getReaction(self):
         m = Reactions.getMouth
         b = Reactions.getBrow
         if m == 'smile':
-            if b == 'smile':
-                return      # Replace each of these returns with separate functions, or just more if with robot feelings
-            elif b == 'neutral':
+            if b == 'b1':
+                print('smile, b1')
+                return      # Replace each of these returns with separate functions, or more ifs with robot feelings
+            elif b == 'b2':
+                print('smile, b2')
                 return
-            elif b == 'frown':
+            elif b == 'b3':
+                print('smile, b3')
                 return
         elif m == 'neutral':
-            if b == 'smile':
+            if b == 'b1':
+                return      # Replace each of these returns with separate functions, or more ifs with robot feelings
+            elif b == 'b2':
                 return
-            elif b == 'neutral':
-                return
-            elif b == 'frown':
+            elif b == 'b3':
                 return
         elif m == 'frown':
-            if b == 'smile':
+            if b == 'b1':
+                return      # Replace each of these returns with separate functions, or more ifs with robot feelings
+            elif b == 'b2':
                 return
-            elif b == 'neutral':
-                return
-            elif b == 'frown':
+            elif b == 'b3':
                 return
 
-
-    def getMouth(self):
+    def getMouth(self):     # Gets most common mouth entry from memory
         if Reactions.ma.count('smile') >= Reactions.ma.count('neutral') & Reactions.ma.count('smile') >= Reactions.ma.count('frown'):  # Priority for positive reactions over negative
             return 'smile'
         elif Reactions.ma.count('neutral') >= Reactions.ma.count('frown') & Reactions.ma.count('neutral') >= Reactions.ma.count('smile'):
@@ -44,11 +46,21 @@ class Reactions:
         elif Reactions.ma.count('frown') >= Reactions.ma.count('neutral') & Reactions.ma.count('frown') >= Reactions.ma.count('smile'):
             return 'frown'
 
-    def getBrow(self):  # OBVIOUSLY NEEDS TO GET CHANGED TO WHATEVER WORDS WE USE FOR BROWS
-        if Reactions.ma.count('smile') >= Reactions.ma.count('neutral') & Reactions.ma.count('smile') >= Reactions.ma.count('frown'):
-            return 'smile'
-        elif Reactions.ma.count('neutral') >= Reactions.ma.count('frown') & Reactions.ma.count('neutral') >= Reactions.ma.count('smile'):
-            return 'neutral'
-        elif Reactions.ma.count('frown') >= Reactions.ma.count('neutral') & Reactions.ma.count('frown') >= Reactions.ma.count('smile'):
-            return 'frown'
+    def getBrow(self):  # 3 states of brow, /  \   -  -   \  /
+        if Reactions.ma.count('b1') >= Reactions.ma.count('b2') & Reactions.ma.count('b1') >= Reactions.ma.count('b3'):
+            return 'b1'
+        elif Reactions.ma.count('b2') >= Reactions.ma.count('b3') & Reactions.ma.count('b2') >= Reactions.ma.count('b1'):
+            return 'b2'
+        elif Reactions.ma.count('b3') >= Reactions.ma.count('b2') & Reactions.ma.count('b3') >= Reactions.ma.count('b1'):
+            return 'b3'
 
+    def updateFace(self, m, b):  # use this to update expressions, m = mouth b = brow
+        Reactions.ma.append(m)
+        Reactions.ba.append(b)
+
+    def debugRandFace(self):    # adds a random mouth and brow to memory
+        import random
+        m = random.choice(Reactions.ml)
+        b = random.choice(Reactions.bl)
+        Reactions.ma.append(m)
+        Reactions.ba.append(b)
