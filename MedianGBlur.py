9baaad55ac8252e7e3919ee86a4f8db2fbd6d0a1
ivand <ivandts@gmail.com>
2018-12-04 14:15:27 +0100

    So it works on the pi

--- MedianGBlur.py
+++ MedianGBlur.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import statistics
 
 def medianBlur(src, outputImage, searchDistance, faceX, faceY, faceW, faceH):
